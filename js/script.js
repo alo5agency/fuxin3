969158c05b87d5e68b92b0c1365420828aab2610
islomabduqodir <islomabduqodir@gmail.com>
2025-02-20 21:39:53 +0500

    finish launch

--- js/script.js
+++ js/script.js
@@ -49,3 +49,40 @@ $(".hover").mouseleave(
   }
 );
 
+// 
+
+document.querySelector("#form-modal button").addEventListener("click", (value) => {
+  value.preventDefault()
+  sendRequest(`#form-modal input[name="name"]`, `#form-modal input[name="phone"]`)
+});
+
+MicroModal.init({
+  openTrigger: 'data-custom-open',
+  closeTrigger: 'data-custom-close',
+  disableScroll: true,
+  disableFocus: true,
+  awaitOpenAnimation: true,
+  awaitCloseAnimation: true
+});
+$('[data-custom-open]').click(function () {
+  $('.modal [name=form]').val($(this).data('form'))
+});
+$('[data-custom-close]').click(function () {
+  $('.modal [name=form]').val('')
+});
+
+function sendRequest(nameSelector, phoneSelector) {
+  let name = document.querySelector(nameSelector).value;
+  let phone = document.querySelector(phoneSelector).value;
+  let requestText = encodeURI(`<b>Заявка</b>\nИмя: ${name}\nНомер: ${phone}`)
+  fetch(`https://api.telegram.org/bot7849947542:AAGEXY5TpjdJfTrRTIiIF6k0SC0-LjfDFZg/sendMessage?text=${requestText}&chat_id=-1002309385088&parse_mode=HTML`)
+}
+
+document.querySelector(".feature-consultation-form button").addEventListener("click", (value) => {
+  value.preventDefault()
+  sendRequest(`.feature-consultation-form input[name="name"]`, `.feature-consultation-form input[name="phone"]`);
+  location.reload();
+});
+
+
+
