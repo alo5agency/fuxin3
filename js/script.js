d2e87c9d4a2d45b3204f36d346d93b3745c3ad60
islomabduqodir <islomabduqodir@gmail.com>
2025-02-26 08:40:00 +0500

    relaunch git commit -m relaunch git commit -m relaunch

--- js/script.js
+++ js/script.js
@@ -21,26 +21,6 @@ var app = function () {
   init();
 }();
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  autoplay: {
-    delay: 5000,
-  },
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-right',
-    prevEl: '.swiper-button-left',
-  },
-});
-
 // hover
 
 $(".hover").mouseleave(
@@ -51,38 +31,18 @@ $(".hover").mouseleave(
 
 // 
 
-document.querySelector("#form-modal button").addEventListener("click", (value) => {
-  value.preventDefault()
-  sendRequest(`#form-modal input[name="name"]`, `#form-modal input[name="phone"]`)
+$(document).ready(function () {
+
+  // Fakes the loading setting a timeout
+  setTimeout(function () {
+    $('body').addClass('loaded');
+  }, 3500);
+
 });
 
-MicroModal.init({
-  openTrigger: 'data-custom-open',
-  closeTrigger: 'data-custom-close',
-  disableScroll: true,
-  disableFocus: true,
-  awaitOpenAnimation: true,
-  awaitCloseAnimation: true
-});
-$('[data-custom-open]').click(function () {
-  $('.modal [name=form]').val($(this).data('form'))
-});
-$('[data-custom-close]').click(function () {
-  $('.modal [name=form]').val('')
-});
 
-function sendRequest(nameSelector, phoneSelector) {
-  let name = document.querySelector(nameSelector).value;
-  let phone = document.querySelector(phoneSelector).value;
-  let requestText = encodeURI(`<b>Заявка</b>\nИмя: ${name}\nНомер: ${phone}`)
-  fetch(`https://api.telegram.org/bot7849947542:AAGEXY5TpjdJfTrRTIiIF6k0SC0-LjfDFZg/sendMessage?text=${requestText}&chat_id=-1002309385088&parse_mode=HTML`)
-}
+//
 
-document.querySelector(".feature-consultation-form button").addEventListener("click", (value) => {
-  value.preventDefault()
-  sendRequest(`.feature-consultation-form input[name="name"]`, `.feature-consultation-form input[name="phone"]`);
-  location.reload();
-});
 
 
 
